e83903a73b7220de915e677cc25c6808c68d57af
ivan_surus <Ivan@sklbook.mks.local>
2017-03-10 00:00:37 +0300

    new develop

--- src/test/java/com/epam/main/Main.java
+++ src/test/java/com/epam/main/Main.java
@@ -3,13 +3,14 @@ package com.epam.main;
 
 import com.epam.driver.DriverSingleton;
 import com.epam.pages.LoginPage;
+import com.epam.pages.SelectSeatPage;
 
 public class Main {
 
     private final static String EMAIL = "tatester@12storage.com";
     private final static String PASSWORD = "qwerty12345";
 
-    public static void main(String[] args) {
+   /* public static void main(String[] args) {
 
         LoginPage loginPage = new LoginPage(DriverSingleton.getDriver());
         loginPage.openPage();
@@ -19,5 +20,18 @@ public class Main {
             e.printStackTrace();
         }
         loginPage.login(EMAIL,PASSWORD);
+    }*/
+
+    public static void main(String[] args) {
+        SelectSeatPage selectSeatPage = new SelectSeatPage(DriverSingleton.getDriver());
+        selectSeatPage.openPage();
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        selectSeatPage.go();
+        selectSeatPage.selectRandomAvailableSeat();
     }
+
 }
--- src/test/java/com/epam/steps/StepsForLoginPage.java
+++ src/test/java/com/epam/steps/StepsForLoginPage.java
@@ -1,14 +1,17 @@
 package com.epam.steps;
 
+import com.epam.pages.LoginPage;
 import com.epam.pages.MainPage;
+import com.epam.pages.SelectSeatPage;
 import org.openqa.selenium.WebDriver;
 import com.epam.driver.DriverSingleton;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class Steps {
+public class StepsForLoginPage {
 
     private WebDriver driver;
+
     private final Logger logger = LogManager.getRootLogger();
 
     public void initBrowser()
@@ -23,15 +26,13 @@ public class Steps {
 
     public void loginWizzAir(String email, String password)
     {
-        MainPage mainPage = new MainPage(driver);
-        mainPage.openPage();
-        mainPage.login(email, password);
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.openPage();
+        loginPage.openSignInForm();
+        loginPage.login(email, password);
     }
 
-    /*public boolean isLoggedIn(String username)
-    {
-        MainPage mainPage = new MainPage(driver);
-        return (mainPage.getLoggedInUserName().trim().equalsIgnoreCase(username));
-    }*/
+
+
 
 }
